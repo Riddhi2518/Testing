67dc8578952ca82e2aa8bc25fb8550e44b787c0b
Riddhi2518 <lakhaniriddhi123@gmail.com>
2025-05-13 14:15:19 +0530

    My First Java

---
+++ Java Testing/src/Basic/Demo.java
@@ -0,0 +1,19 @@
+package Basic;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a= 10 , b= 20;
+        int sum= a+b; 
+        int mul= a*b;
+        double div= (double)a/b;
+        float sub= a-b;
+        
+        System.out.println("Sum is: " +sum);
+        System.out.println("Multiplication is: " +mul);   
+        System.out.println("Division is: " +div);
+        System.out.println("Substration is: " +sub);
+	}
+
+}
